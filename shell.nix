4090464f45fc20fc69a646a5a8b09118d1336f07
BWbwchen <tim.chenbw@gmail.com>
2023-11-04 16:25:03 +0800

    [build  ]: add nix support, with rustc version 1.72.1
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import (fetchTarball
+    # ("https://github.com/NixOS/nixpkgs/archive/621f51253edffa1d6f08d5fce4f08614c852d17e.tar.gz"))
+    ("https://github.com/NixOS/nixpkgs/archive/c423f8f656196f9b955f506ce91199e24b180bfa.tar.gz"))
+  # ("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz"))
+    { };
+
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell { buildInputs = [ pkgs.cargo pkgs.rustc ]; }
